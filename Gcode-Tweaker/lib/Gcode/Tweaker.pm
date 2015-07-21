b1b24b971c11a7647a73609a9e8ccf2dcaf7b338
yulivee <yulivee@users.noreply.github.com>
2015-07-21 09:27:21 +0200

    paar files vergessen

--- Gcode-Tweaker/lib/Gcode/Tweaker.pm
+++ Gcode-Tweaker/lib/Gcode/Tweaker.pm
@@ -4,11 +4,19 @@ use Dancer2;
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+
+	my @test1 = qw(anna otto hans);
+
+    template 'index', {
+	test1 => \@test1,
+	};
 };
 
-get '/welcome' => sub {
-	return 'Hello World';
+get '/temperature' => sub {
+	template 'temperature';
 };
 
+get '/tensile' => sub {
+	template 'tensile';
+};
 true;
